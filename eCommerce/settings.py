2b782ddee8bafefb7276db8ece42e5b86556fe33
alchemist2016 <vukdragovic@hotmail.com>
2020-04-21 13:41:45 +0000

    Remove default secret key

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -32,7 +32,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECURITY_KEY', 'ba&by@8snpxvgldfd8sxvaf316acs8+3@-5673#)jpc5gie^mw')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
