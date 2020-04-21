498dc029c253e65c9545632faf523cbfe983ac21
alchemist2016 <vukdragovic@hotmail.com>
2020-04-21 15:51:40 +0000

    Updated user registration form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from accounts.models import UserProfileInfo
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserForm(forms.ModelForm):
@@ -9,6 +10,19 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username','password','email')
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError("Email address must be unique")
+        return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if not password:
+            raise ValidationError("Please confirm your password!")
+        return password
+        
         
 class UserProfileInfoForm(forms.ModelForm):
      class Meta():
