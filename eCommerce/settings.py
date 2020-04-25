b4fe24328d28731fc96abb8d48d3b6e8a11a3cb4
alchemist2016 <vukdragovic@hotmail.com>
2020-04-25 16:49:52 +0000

    Add changes in login files

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -148,6 +149,7 @@ STATICFILES_DIRS = [
     STATIC_DIR,
     os.path.join(BASE_DIR, "static"),
 ]
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
