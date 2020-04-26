b7330364f1ef0b2b841d8ebcac0687ce1c3adf9f
alchemist2016 <vukdragovic@hotmail.com>
2020-04-26 12:02:09 +0000

    Fixing the bug whit naming in Middleware

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'witenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'eCommerce.urls'
