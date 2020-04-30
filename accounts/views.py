26a0d5db4078cd6e502f68bf031a44d5c916d4f9
alchemist2016 <vukdragovic@hotmail.com>
2020-04-30 09:28:20 +0000

    Collected static for AWS

--- accounts/views.py
+++ accounts/views.py
@@ -26,7 +26,7 @@ def register(request):
     registered = False
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
-        
+
         if user_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
@@ -36,7 +36,7 @@ def register(request):
             print(user_form.errors)
     else:
         user_form = UserForm()
-        
+
     return render(request, 'registration.html',
                            {'user_form': user_form,
                             'registered': registered})
--- cart/views.py
+++ cart/views.py
@@ -10,17 +10,17 @@ def view_cart(request):
 
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
-    
+
     if (int(request.POST.get('quantity'))) == 0:
         return redirect(reverse('index'))
     else:
         quantity = int(request.POST.get('quantity'))
-    
+
     cart = request.session.get('cart', {})
     if id in cart:
-        cart[id] = int(cart[id]) + quantity      
+        cart[id] = int(cart[id]) + quantity
     else:
-        cart[id] = cart.get(id, quantity) 
+        cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
     return redirect(reverse('index'))
